6061acdb7878327d668a3d420b05a7b17fe93cd2
psw221 <tpdnddlek@gmail.com>
2016-05-17 23:54:07 +0900

    fix - sewuong
    change code

--- Battle/BattleManager.cs
+++ Battle/BattleManager.cs
@@ -19,6 +19,7 @@ public class BattleManager {
      */
 
     // 여기도 바꾸고
+    // 내가 한번 바꿔봄 - 박세웅
 	// Update is called once per frame
 	public void CheckBattle () {    //todo : 이부분을 호출하는 부분이 필요함
         if (normalAttackTime != 0)
